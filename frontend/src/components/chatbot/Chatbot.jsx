fd7332ff1246921e77edc861d4781bf7fb782ab6
Aditya Sakpal <112710558+Aditya-Sakpal@users.noreply.github.com>
2024-01-09 13:02:50 +0530

    Update Chatbot.jsx

--- frontend/src/components/chatbot/Chatbot.jsx
+++ frontend/src/components/chatbot/Chatbot.jsx
@@ -53,7 +53,7 @@ function Chatbot() {
     ]);
     setCurrentMessage('Bot is typing ...')
     setIsInputDisabled(true)
-    const response = await axios.post("http://localhost:4000/chats", { query: currentMessage })
+    const response = await axios.post("https://portfolio2-backend-xi.vercel.app/chats", { query: currentMessage })
     console.log(response)
     if (response.data.output) {
       setMessages((prevMessages) => [
