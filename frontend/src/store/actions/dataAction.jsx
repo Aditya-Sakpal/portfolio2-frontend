99dacf05061f622aa1f939694deb75f07ec89c4c
Aditya Sakpal <112710558+Aditya-Sakpal@users.noreply.github.com>
2024-01-14 16:30:09 +0530

    Update dataAction.jsx

--- frontend/src/store/actions/dataAction.jsx
+++ frontend/src/store/actions/dataAction.jsx
@@ -8,7 +8,7 @@ export const fetchData = () => {
         await axios.get('https://portfolio2-backend-xi.vercel.app/data')
             .then(res => {
                 console.log(res.data)
-                // localStorage.setItem("data",JSON.stringify(res.data))
+                localStorage.setItem("data",JSON.stringify(res.data))
                 dispatch({ type: actions.FETCH_DATA_SUCCESS, payload: res.data });
             })
             .catch(e => {
